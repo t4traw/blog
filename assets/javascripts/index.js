3b6d1ef01fa2c1c3f0f69602133384a965c8aa1a
t4traw <t4traw@gmail.com>
2018-08-10 23:05:10 +0900

    yarnでいれたパッケージを有効化

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,2 +1,7 @@
+import ScrollTrigger from '../../node_modules/scrolltrigger-classes/ScrollTrigger.js'
 import '../stylesheets/index.css.sass'
 import './menu_button.js'
+
+document.addEventListener('DOMContentLoaded', function () {
+  var trigger = new ScrollTrigger()
+})
