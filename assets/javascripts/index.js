a03111c7a56af31c1abda7cfc52551db71f51c42
Tatsuro Moriyama <t4traw@gmail.com>
2019-04-24 11:51:52 +0900

    記事部分のデザイン変更

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,10 +1,2 @@
-import ScrollTrigger from '../../node_modules/scrolltrigger-classes/ScrollTrigger.js'
-// import InstantClick from '../../node_modules/instantclick/instantclick.js'
-import '../stylesheets/index.css.sass'
+import '../stylesheets/index.css.scss'
 import './menu_button.js'
-
-// InstantClick.init()
-
-document.addEventListener('DOMContentLoaded', function () {
-  var trigger = new ScrollTrigger()
-})
