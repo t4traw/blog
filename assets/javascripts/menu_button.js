978f05ccafdcdefb3313fe1a7e7e7a7ae315dd93
TatsuroMoriyama <t4traw@gmail.com>
2019-06-13 10:04:00 +0900

    変数名の変更

--- assets/javascripts/menu_button.js
+++ assets/javascripts/menu_button.js
@@ -1,9 +1,9 @@
 function toggle_menu(el){
   const target = el.dataset.target;
-  const $target = document.getElementById(target);
+  const menu = document.getElementById(target);
 
   // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
   el.classList.toggle('is-active');
-  $target.classList.toggle('is-active');
+  menu.classList.toggle('is-active');
 }
 window.toggle_menu = toggle_menu
