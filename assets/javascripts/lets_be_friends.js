b93dca4cac3656ce3ec50765c77e1006ac7e144b
TatsuroMoriyama <t4traw@gmail.com>
2019-08-09 16:58:47 +0900

    プロフィール画像がない所でjsのエラーがでている問題を修正

--- assets/javascripts/lets_be_friends.js
+++ assets/javascripts/lets_be_friends.js
@@ -1,7 +1,11 @@
-let txt = document.getElementById('lets_be_friends').innerHTML.split('')
-document.getElementById('lets_be_friends').innerHTML = ''
-for (i = 0; i < txt.length; i++) {
-  let span = document.createElement('span')
-  span.innerHTML = txt[i]
-  document.getElementById('lets_be_friends').appendChild(span)
+let txt = document.getElementById('lets_be_friends')
+if (txt != void 0) {
+  txt = txt.innerHTML.split('')
+  document.getElementById('lets_be_friends').innerHTML = ''
+  for (i = 0; i < txt.length; i++) {
+    let span = document.createElement('span')
+    span.innerHTML = txt[i]
+    document.getElementById('lets_be_friends').appendChild(span)
+  }
 }
+
