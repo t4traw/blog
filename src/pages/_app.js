9b293423933f6995a8a63ba7804ca2cd176fc6a6
t4traw <t4traw@gmail.com>
2021-08-12 13:08:57 +0900

    GoogleAnalyticsの導入

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,22 @@ import '../../styles/head.scss'
 import '../../styles/article.scss'
 import '../../styles/baloon.scss'
 import '../../styles/ptcg.scss'
+import * as gtag from '../lib/gtag'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
