dd9e565de73c5f9d0427d7d78303fd0a96078572
t4traw <t4traw@gmail.com>
2021-08-15 18:47:43 +0900

    padding調整

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout ({ children, pageTitle, ...props }) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>{pageTitle}</title>
       </Head>
-      <section className='container mx-auto pb-48 px-4 md:px-16 lg:px-48 xl:px-64'>
+      <section className='container mx-auto pb-48 px-4 md:px-20 lg:px-52 xl:px-72'>
         <Header />
         <div className='layout'>
           <div className='content'>{children}</div>
